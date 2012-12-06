53440486d2ec1407102e168b42a741eec253c21c
Ken Morishita <mokemokechicken@gmail.com>
2012-12-06 15:44:51 +0900

    change image save timing

--- src/public/js/handwrite.js
+++ src/public/js/handwrite.js
@@ -93,7 +93,6 @@ HW.create = function() {
     
     var drawStrokes = function(strokes, ys) {
         var context = context2d;
-        imageData = context.getImageData(0, 0, view.canvas.width(), view.canvas.height());
 
         context.lineWidth = that.options.pw/2;
         context.strokeStyle = "red";
@@ -204,6 +203,7 @@ HW.create = function() {
     
     var mouseup = function() {
         model.isDown = false;
+        imageData = context.getImageData(0, 0, view.canvas.width(), view.canvas.height());
         repository.infer(model,{
             size: [view.canvas.width(), view.canvas.height()]
         }, drawStrokes, false);

cc19063da6b48a801f8f8c6eadcb3514aa1ce053
Ken Morishita <mokemokechicken@gmail.com>
2012-12-12 19:21:00 +0900

    modify

---
+++ src/configservice_client/config.py
@@ -0,0 +1,27 @@
+# coding: utf8
+'''
+Created on 2012/12/12
+
+@author: k_morishita
+'''
+
+
+import copy
+import time
+
+from configservice_client.client import ConfigClient
+
+CONFIG_SERVER_URL = "http://localhost:8000/config"
+
+_CONFIG_CACHE = {}
+_CONFIG_CACHE_TIME = {}
+CACHE_EXPIRE = 10
+
+def load_config(charset_type):
+    if charset_type not in _CONFIG_CACHE or _CONFIG_CACHE_TIME[charset_type]+CACHE_EXPIRE < time.time():
+        cc = ConfigClient(CONFIG_SERVER_URL)
+        _CONFIG_CACHE[charset_type] = cc.get_config(charset_type)
+        _CONFIG_CACHE_TIME[charset_type] = time.time()
+    return copy.copy(_CONFIG_CACHE[charset_type])
+
+
--- src/handwrite_web/data_config.py
+++ src/handwrite_web/data_config.py
@@ -6,23 +6,7 @@ Created on 2012/11/30
 '''
 
 from __future__ import absolute_import
-
-import copy
-import time
-from configservice.client.client import ConfigClient
-
-CONFIG_SERVER_URL = "http://localhost:8000/config"
-
-_CONFIG_CACHE = {}
-_CONFIG_CACHE_TIME = {}
-CACHE_EXPIRE = 10
-
-def load_config(charset_type):
-    if charset_type not in _CONFIG_CACHE or _CONFIG_CACHE_TIME[charset_type]+CACHE_EXPIRE < time.time():
-        cc = ConfigClient(CONFIG_SERVER_URL)
-        _CONFIG_CACHE[charset_type] = cc.get_config(charset_type)
-        _CONFIG_CACHE_TIME[charset_type] = time.time()
-    return copy.copy(_CONFIG_CACHE[charset_type])
+from configservice_client.config import load_config
 
 def get_chars(charset_type):
     return load_config(charset_type)["chars"]
